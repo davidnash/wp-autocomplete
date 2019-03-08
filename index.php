b46e18347b26146778696c442c7b8caab3458a7b
David Nash <274968+davidnash@users.noreply.github.com>
2019-03-08 11:16:47 +1100

    Add very simple autocomplete code with hardcoded list

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
 <?php get_header(); ?>
+
 Let's get ready to autocomplete!
+
+<input class="awesomplete"
+       data-list="Ada, Java, JavaScript, Brainfudge, LOLCODE, Node.js, Ruby on Rails" />
+
 <?php get_footer(); ?>
