d8e0cffd572667153cb393c12b793830777632fb
David Nash <274968+davidnash@users.noreply.github.com>
2019-03-11 11:51:12 +1100

    Read autocomplete values from the database
    And add basic header.php and footer.php

---
+++ footer.php
@@ -0,0 +1,11 @@
+<footer>
+    <!-- Quick and dirty spacing -->
+    <br /><br /><br /><br />
+    <hr>
+    <p><small>By David Nash. For the full post, see <a href="https://davidnash.com.au/create-an-autocomplete-field-in-wordpress-2019-update">Create an autocomplete field in WordPress: 2019 Update</a></small></p>
+</footer>
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<title><?php wp_title(); ?></title>
+	<meta charset="utf-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <?php wp_head(); ?>
+</head>
+<body>
+    <h2>Let's autocomplete!</h2>
