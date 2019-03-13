738b6cf9e039fb9450f346b591002c46c5a4c165
David Nash <274968+davidnash@users.noreply.github.com>
2019-03-13 14:30:10 +1100

    Load wp-autocomplete.js from wordpress

--- functions.php
+++ functions.php
@@ -2,6 +2,7 @@
 function add_theme_scripts() {
     wp_enqueue_style('awesomepletecss', get_template_directory_uri() . '/awesomplete/awesomplete.css');
     wp_enqueue_script('awesompletejs', get_template_directory_uri() . '/awesomplete/awesomplete.js');
+    wp_enqueue_script('theme-js', get_template_directory_uri() . '/js/wp-autocomplete.js', ['awesompletejs'] );
 }
 
 add_action('wp_enqueue_scripts', 'add_theme_scripts');
---
+++ js/wp-autocomplete.js
@@ -0,0 +1,3 @@
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('wp-autocomplete.js loaded!');
+});
